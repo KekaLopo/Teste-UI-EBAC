6dd2895f788a77c1cd8605705439373b9fa862a0
Keka <kekalopo@gmail.com>
2022-10-04 19:25:50 -0300

    testando massa de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/example.json') // testando fixture com variavel
 
     context('Funcionalidade Login', ()=>{
         it('Deve fazer login com sucesso',()=>{
@@ -10,19 +11,32 @@
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá')
         })
 
+        //testanto fixture acessando pelo arquivo
+        it.only('Deve fazer Login com sucesso - Usando Fixture',()=>{
+            cy.fixture('example').then(dados => {
+                cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+                cy.get('#username').type(dados.usuario)
+                cy.get('#password').type(dados.senha)
+                cy.get('.woocommerce-form > .button').click()
+                cy.get('.page-title').should('have.text','Minha conta')
+                cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá')
+            })
+        })
 
         it('Deve exibir uma mensagem de erro ao inserir usuario invalido ', (    
         )=>{
             cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-            cy.get('#username').type('aluno_@teste')
-            cy.get('#password').type('teste@teste.com')
+            cy.get('#username').type(perfil.usuario+a)
+            cy.get('#password').type(perfil.senha)
             cy.get('.woocommerce-form > .button').click()
             cy.get('.woocommerce-error > li').should('contain','Erro')
         })
 
+    
+
         it('Deve exibir uma mensagem de erro ao inserir senha invalida ', (    
             )=>{
-                cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+                cy.visit('http://lojaebac.ebaconline.art.brminha-conta/')
                 cy.get('#username').type('aluno_ebac@teste.com')
                 cy.get('#password').type('teste@teste')
                 cy.get('.woocommerce-form > .button').click()
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -10,26 +10,24 @@ describe('Funcionalidade Pagina de Produtos', () => {
 
     it('Deve selecionar um produto da Lista', () => {
         cy.get('[class="product-block grid"]')
-            .first()
+            .first() 
             .click()
 
-        //posso usar eq() pra pegar o item que eu quiser começando da posição 0 
-        //ou posso usar contains ('e passar o nome dele aqui')
+            //posso usar eq() pra pegar o item que eu quiser começando da posição 0 
+            //ou posso usar contains ('e passar o nome dele aqui')
     });
 
     it.only('Deve Adicionar um produto ao Carrinho', () => {
-        let quantidade = 3;
-        cy.get('.dropdown-toggle > .mini-cart-items')
+        let quantidade= 3;
+    cy.get('.dropdown-toggle > .mini-cart-items')
         cy.get('[class="product-block grid"]')
-            .contains('Abominable Hoodie')
-            .click()
+        .contains('Abominable Hoodie')
+        .click()
         cy.get('.button-variable-item-M').click()
         cy.get('.button-variable-item-Green').click()
         cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', quantidade+" x Abominable Hoodie")
-
-    }
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain',quantidade)
+        cy.get('Woocommerce-message').should('contains', quantidade+" x Abominable Hoodie")
     });
 });
